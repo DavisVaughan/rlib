64eead1fada0271037ddce9f98064679278d3b64
DavisVaughan <davis@rstudio.com>
2020-04-05 18:45:09 -0400

    Reset

--- inst/include/rlib/html/search/all_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['r_5fint_5fderef_0',['r_int_deref',['../vec-int_8h.html#ac9cf51214224036b72277b2accda90ed',1,'vec-int.h']]],
-  ['r_5fnew_5fint_1',['r_new_int',['../vec-int_8h.html#a7ad418074750458273bfee1209316c8e',1,'vec-int.h']]]
-];
--- inst/include/rlib/html/search/files_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['vec_2dint_2eh_3',['vec-int.h',['../vec-int_8h.html',1,'']]]
-];
