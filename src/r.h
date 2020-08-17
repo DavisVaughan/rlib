902d1b0ed99dee60602bb83b4e7b57d99395c228
DavisVaughan <davis@rstudio.com>
2020-08-17 14:27:01 -0400

    Add more vec utilities

--- src/r.h
+++ src/r.h
@@ -4,9 +4,11 @@
 
 #define R_NO_REMAP
 #include <R.h>
+#include <Rversion.h>
 #include <Rinternals.h>
 #include <stdbool.h>
 
+// -----------------------------------------------------------------------------
 
 typedef SEXP sexp;
 
@@ -20,6 +22,8 @@ typedef R_len_t r_short_ssize_t;
 
 #define r_null R_NilValue
 
+// -----------------------------------------------------------------------------
+
 #define KEEP PROTECT
 #define FREE UNPROTECT
 
@@ -28,5 +32,24 @@ typedef R_len_t r_short_ssize_t;
 #define KEEP_WITH_INDEX(x, p_i) PROTECT_WITH_INDEX(x, p_i)
 #define REKEEP REPROTECT
 
+// -----------------------------------------------------------------------------
+// Compatibility
+
+#if (R_VERSION < R_Version(3, 5, 0))
+# define DATAPTR_RO(x) ((const void*) DATAPTR(x))
+# define LOGICAL_RO(x) ((const int*) LOGICAL(x))
+# define INTEGER_RO(x) ((const int*) INTEGER(x))
+# define REAL_RO(x) ((const double*) REAL(x))
+# define COMPLEX_RO(x) ((const r_complex_t*) COMPLEX(x))
+# define STRING_PTR_RO(x) ((const sexp*) STRING_PTR(x))
+# define RAW_RO(x) ((const r_byte_t*) RAW(x))
+#endif
+
+// -----------------------------------------------------------------------------
+
+// Const pointer for fast list access
+#define VECTOR_PTR_RO(x) ((const sexp*) DATAPTR_RO(x))
+
+// -----------------------------------------------------------------------------
 
 #endif
--- src/vec.h
+++ src/vec.h
@@ -1,14 +1,107 @@
 #ifndef RLIB_VEC_H
 #define RLIB_VEC_H
 
-
 #include "r.h"
 #include "type.h"
 
+// -----------------------------------------------------------------------------
 
 static inline sexp r_new_vector(enum r_type type, r_ssize_t n) {
   return Rf_allocVector(type, n);
 }
 
+static inline sexp r_new_lgl(r_ssize_t n) {
+  return r_new_vector(r_type_logical, n);
+}
+static inline sexp r_new_int(r_ssize_t n) {
+  return r_new_vector(r_type_integer, n);
+}
+static inline sexp r_new_dbl(r_ssize_t n) {
+  return r_new_vector(r_type_double, n);
+}
+static inline sexp r_new_chr(r_ssize_t n) {
+  return r_new_vector(r_type_character, n);
+}
+static inline sexp r_new_raw(r_ssize_t n) {
+  return r_new_vector(r_type_raw, n);
+}
+static inline sexp r_new_cpl(r_ssize_t n) {
+  return r_new_vector(r_type_complex, n);
+}
+static inline sexp r_new_lst(r_ssize_t n) {
+  return r_new_vector(r_type_list, n);
+}
+
+// -----------------------------------------------------------------------------
+
+static inline sexp r_new_scalar_lgl(int x) {
+  return Rf_ScalarLogical(x);
+}
+static inline sexp r_new_scalar_int(int x) {
+  return Rf_ScalarInteger(x);
+}
+static inline sexp r_new_scalar_dbl(double x) {
+  return Rf_ScalarReal(x);
+}
+static inline sexp r_new_scalar_chr(sexp x) {
+  return Rf_ScalarString(x);
+}
+static inline sexp r_new_scalar_raw(r_byte_t x) {
+  return Rf_ScalarRaw(x);
+}
+static inline sexp r_new_scalar_cpl(r_complex_t x) {
+  return Rf_ScalarComplex(x);
+}
+// static inline sexp r_new_scalar_lst(sexp x) {
+//
+// }
+
+// -----------------------------------------------------------------------------
+
+static inline int* r_lgl_deref(sexp x) {
+  return LOGICAL(x);
+}
+static inline int* r_int_deref(sexp x) {
+  return INTEGER(x);
+}
+static inline double* r_dbl_deref(sexp x) {
+  return REAL(x);
+}
+// static inline sexp* r_chr_deref(sexp x) {
+//   return STRING_PTR(x);
+// }
+static inline r_byte_t* r_raw_deref(sexp x) {
+  return RAW(x);
+}
+static inline r_complex_t* r_cpl_deref(sexp x) {
+  return COMPLEX(x);
+}
+// static inline sexp* r_lst_deref(sexp x) {
+//   return VECTOR_PTR(x);
+// }
+
+static inline const int* r_lgl_const_deref(sexp x) {
+  return LOGICAL_RO(x);
+}
+static inline const int* r_int_const_deref(sexp x) {
+  return INTEGER_RO(x);
+}
+static inline const double* r_dbl_const_deref(sexp x) {
+  return REAL_RO(x);
+}
+static inline const sexp* r_chr_const_deref(sexp x) {
+  return STRING_PTR_RO(x);
+}
+static inline const r_byte_t* r_raw_const_deref(sexp x) {
+  return RAW_RO(x);
+}
+static inline const r_complex_t* r_cpl_const_deref(sexp x) {
+  return COMPLEX_RO(x);
+}
+static inline const sexp* r_lst_const_deref(sexp x) {
+  return VECTOR_PTR_RO(x);
+}
+
+// -----------------------------------------------------------------------------
 
 #endif
